eda68dc7d2e1b1af63d2b4acd26bdcb8930ad1dd
Vinay Patel <vinayspatel0@gmail.com>
2025-01-06 15:08:33 +0000

    feat: create FavouritesFragments java file including empty constructor and onCreatView method

---
+++ app/src/main/java/com/northcoders/datafilteringinandroid/FavouritesFragment.java
@@ -0,0 +1,23 @@
+package com.northcoders.datafilteringinandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavouritesFragment extends Fragment {
+
+    public FavouritesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favourites, container, false);
+    }
+}
