7cbb9a2b7724cc05ed35cd9958f7f756b927f4fb
Vinay Patel <vinayspatel0@gmail.com>
2025-01-06 16:27:17 +0000

    feat: implement NavigationBarView.OnItemSelectedListener, create NavigationBarView object findViewById, setOnItemSelectedListener and setSlectedItemId . Create conditional logic to check whether MenuItem in onNavigationItemSelected method equal to id of the menu item created for the navigation bar

--- app/src/main/java/com/northcoders/datafilteringinandroid/MainActivity.java
+++ app/src/main/java/com/northcoders/datafilteringinandroid/MainActivity.java
@@ -1,24 +1,67 @@
 package com.northcoders.datafilteringinandroid;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView
+
+        .OnItemSelectedListener{
+
+    NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    AddFragment addFragment = new AddFragment();
+    FavouritesFragment favouritesFragment = new FavouritesFragment();
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.home){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main,homeFragment)
+                    .commit();
+            return true;
+        }
+        if(item.getItemId() == R.id.add){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main,addFragment)
+                    .commit();
+            return true;
+        }
+        if(item.getItemId() == R.id.favourites){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main,favouritesFragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
     }
 }
